d8097ce1d3fbb8ecc3c0291ffa61dac2a0feca43
KushalTonystarkC <sauryachoudhary7@gmail.com>
2024-01-02 08:31:48 +0530

    Created Login Page

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 import 'package:testproject/pages/home_page.dart';
-// import HomePage from "./home_page.dart"
+import 'package:testproject/pages/login_page.dart';
 
 void main() {
   runApp(const MyApp());
@@ -13,7 +14,21 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: HomePage(),
+      themeMode: ThemeMode.light,
+      theme: ThemeData(
+          colorScheme: ColorScheme.fromSeed(
+            seedColor: Colors.deepPurple,
+          ),
+          useMaterial3: true,
+          fontFamily: GoogleFonts.lato().fontFamily
+          // primaryTextTheme: GoogleFonts.latoTextTheme()
+          ),
+      darkTheme: ThemeData(brightness: Brightness.dark),
+      routes: {
+        "/": (context) => LoginPage(),
+        "/home": (context) => HomePage(),
+        "/login": (context) => LoginPage()
+      },
     );
   }
 }
--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -5,6 +5,7 @@ class HomePage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
+        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
         title: Text("First App"),
       ),
       body: Center(
